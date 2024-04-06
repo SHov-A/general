a0f0aa3847c004493c91f18a4622938c1e50b766
styopahovhannisyan <styopa.hovhannisyan@inomma.com>
2024-04-06 22:09:41 +0400

    minor changs

--- README.md
+++ README.md
@@ -6,9 +6,9 @@
     <li>Maven v3.8.3</li>
     <li>browser drivers - all the latest versions</li>
 </ul>
-For running this project choose in <code>pom.xml</code> file class path and write <code>mvn clean verify -Dstar.acceptance.url=https://storedev.startrekfleetcommand.com/ -Dstar.acceptance.driver=org.openqa.selenium.chrome.ChromeDriver</code> in terminal. If you wont to run by another browser, then change <code>star.acceptance.driver</code>.
+For running this project choose in <code>pom.xml</code> file class path and write <code>mvn clean verify -Dstar.acceptance.url=https:// -Dstar.acceptance.driver=org.openqa.selenium.chrome.ChromeDriver</code> in terminal. If you wont to run by another browser, then change <code>star.acceptance.driver</code>.
 <br>
-For running this project for parallel mode write <code>mvn clean verify -Dparallel.tests.count=10 -Dstar.acceptance.url=https://storedev.startrekfleetcommand.com/ -Dstar.acceptance.driver=org.openqa.selenium.chrome.ChromeDriver</code> in terminal. If you wont to run by another count, then change <code>parallel.test.count</code>.
+For running this project for parallel mode write <code>mvn clean verify -Dparallel.tests.count=10 -Dstar.acceptance.url=https:// -Dstar.acceptance.driver=org.openqa.selenium.chrome.ChromeDriver</code> in terminal. If you wont to run by another count, then change <code>parallel.test.count</code>.
 <br>
 After running test report is generated in <code>target</code> folder with name <code>cucumber-jvm-reports</code>, if test fails the screen shot of related page appears in report.
 <br>
--- run_tests.sh
+++ run_tests.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 #staging
-mvn clean verify -Dparallel.tests.count=5 -Dstar.acceptance.url=https://storestaging.startrekfleetcommand.com/ -Dstar.acceptance.driver=org.openqa.selenium.chrome.ChromeDriver
+mvn clean verify -Dparallel.tests.count=5 -Dstar.acceptance.url=https:// -Dstar.acceptance.driver=org.openqa.selenium.chrome.ChromeDriver
 
-mvn clean test -Dstar.acceptance.url=https://storestaging.startrekfleetcommand.com/ -Dstar.acceptance.driver=org.openqa.selenium.firefox.FirefoxDriver
+mvn clean test -Dstar.acceptance.url=https:// -Dstar.acceptance.driver=org.openqa.selenium.firefox.FirefoxDriver
 
-mvn clean test -Dstar.acceptance.url=https://storestaging.startrekfleetcommand.com/ -Dstar.acceptance.driver=org.openqa.selenium.edge.EdgeDriver
+mvn clean test -Dstar.acceptance.url=https:// -Dstar.acceptance.driver=org.openqa.selenium.edge.EdgeDriver
 
 #dev
-mvn clean verify -Dparallel.tests.count=5 -Dstar.acceptance.url=https://storedev.startrekfleetcommand.com/ -Dstar.acceptance.driver=org.openqa.selenium.chrome.ChromeDriver
+mvn clean verify -Dparallel.tests.count=5 -Dstar.acceptance.url=https:// -Dstar.acceptance.driver=org.openqa.selenium.chrome.ChromeDriver
 
-mvn clean test verify -Dparallel.tests.count=5 -Dstar.acceptance.url=https://storedev.startrekfleetcommand.com/ -Dstar.acceptance.driver=org.openqa.selenium.firefox.FirefoxDriver
+mvn clean test verify -Dparallel.tests.count=5 -Dstar.acceptance.url=https:// -Dstar.acceptance.driver=org.openqa.selenium.firefox.FirefoxDriver
 
-mvn clean test verify -Dparallel.tests.count=5 -Dstar.acceptance.url=https://storedev.startrekfleetcommand.com/ -Dstar.acceptance.driver=org.openqa.selenium.edge.EdgeDriver
+mvn clean test verify -Dparallel.tests.count=5 -Dstar.acceptance.url=https:// -Dstar.acceptance.driver=org.openqa.selenium.edge.EdgeDriver
 
 #remote
-mvn clean verify -Dparallel.tests.count=5 -Dstar.acceptance.url=https://storedev.startrekfleetcommand.com/ -Dstar.acceptance.grid.url=http://10.245.8.170:4444/wd/hub/ -Dstar.acceptance.browser=chrome -Dstar.acceptance.version=96.0
+mvn clean verify -Dparallel.tests.count=5 -Dstar.acceptance.url=https:// -Dstar.acceptance.grid.url=http://10.245.8.170:4444/wd/hub/ -Dstar.acceptance.browser=chrome -Dstar.acceptance.version=96.0
 
-mvn clean verify -Dparallel.tests.count=5 -Dstar.acceptance.url=https://storedev.startrekfleetcommand.com/ -Dstar.acceptance.grid.url=http://10.245.8.170:4444/wd/hub/ -Dstar.acceptance.browser=firefox -Dstar.acceptance.version=94.0
+mvn clean verify -Dparallel.tests.count=5 -Dstar.acceptance.url=https:// -Dstar.acceptance.grid.url=http://10.245.8.170:4444/wd/hub/ -Dstar.acceptance.browser=firefox -Dstar.acceptance.version=94.0
